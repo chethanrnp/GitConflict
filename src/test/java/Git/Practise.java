05a270992497516596f2929ddcc644d6eaf5ae00
chethanrnp <chethan.supernova@gmail.com>
2023-08-02 16:51:17 +0530

    conflict

---
+++ src/test/java/Git/Practise.java
@@ -0,0 +1,12 @@
+package Git;
+
+import org.testng.annotations.Test;
+
+public class Practise {
+
+	@Test
+	public void practise() {
+		System.out.println("Chetahn");
+		System.out.println("Conflict");
+	}
+}
